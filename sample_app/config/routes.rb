d2c3d081051e24513e2a361ad315bca5b5095a8a
Constantin Câmpean <mail.la.costi@gmail.com>
2022-07-01 19:43:52 +0300

    prepare the '/signup' route

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  # Defines the root path route ("/")
+  root "static_pages#home"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/about", to: "static_pages#about"
   get "/help", to: "static_pages#help"
   get "/contact", to: "static_pages#contact"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root "static_pages#home"
+  get  "/signup",  to: "users#new"
 end
